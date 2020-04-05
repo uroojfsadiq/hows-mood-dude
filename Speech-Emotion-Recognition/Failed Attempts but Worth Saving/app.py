f48b088c117cebd1a32e14257724d13dfa4ac702
Urooj Fatima <uroojfsadiq@gmail.com>
2020-04-05 17:35:28 +0500

    yeh kab khatam hoga yar?

---
+++ Speech-Emotion-Recognition/Failed Attempts but Worth Saving/app.py
@@ -0,0 +1,49 @@
+import os
+import urllib.request
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+import pickle
+import pandas as pd
+
+with open(f'model/pred.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+UPLOAD_FOLDER = './uploads'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['wav'])
+
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected for uploading')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			flash('File successfully uploaded')
+			return  redirect('/')
+		else:
+			flash('Allowed file type is just wav for now')
+			return redirect(request.url)
+
+if __name__ == "__main__":
+    app.run()
